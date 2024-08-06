a92d81d7c2af2eb0a3725ac80dedbbafa53ff96b
Akshat Gupta <131291259+guptaa-akshat@users.noreply.github.com>
2024-08-06 22:48:41 +0530

    json

---
+++ parse_resume.py
@@ -0,0 +1,73 @@
+{
+  "Name": "Akshat Gupta",
+  "Contact Information": {
+    "Email": "guptaaa.akshat@gmail.com",
+    "Phone": "+91-9877035733",
+    "Location": "Jalandhar, Punjab"
+  },
+  "Professional Summary": "",
+  "Work Experience": [],
+  "Education": [
+    {
+      "Degree": "Master's of Technology (Machine Learning & Artificial Intelligence)",
+      "Institution": "Lovely Professional University (LPU)",
+      "Location": "Phagwara, Punjab",
+      "Graduation Date": "2023 - Present",
+      "CGPA": 8.5
+    },
+    {
+      "Degree": "Bachelor's of Technology (Electronics and Communication Engineering)",
+      "Institution": "DAV Institute of Engineering and Technology",
+      "Location": "Jalandhar, Punjab",
+      "Graduation Date": "2018 - 2022",
+      "CGPA": 8.15
+    }
+  ],
+  "Skills": [
+    "Python",
+    "Numpy",
+    "Pandas",
+    "Matplotlib",
+    "OpenCV",
+    "TensorFlow",
+    "ScikitLearn",
+    "Pytorch",
+    "Jupyter Notebook",
+    "VS Code"
+  ],
+  "Certifications": [
+    "IBM Certified Data Science with Python",
+    "IBM Certified Machine Learning with Python",
+    "A.I with Python - Itronix Solutions",
+    "Certification in Python language - Udemy, Coursera"
+  ],
+  "Languages": [],
+  "Additional Information": {
+    "Projects": [
+      {
+        "Title": "An A.I Virtual Assistant",
+        "Technologies": ["Python", "Machine Learning Libraries"],
+        "Description": "Designed and developed an AI virtual assistant with features like home automation, NLP algorithms, speech recognition, and task automation."
+      },
+      {
+        "Title": "Pose Detection using ML",
+        "Technologies": ["Python", "Machine Learning"],
+        "Description": "Developed a robust pose detection system with visualization techniques to enhance user experience."
+      },
+      {
+        "Title": "Mobile Health Application using IoT",
+        "Technologies": ["IoT"],
+        "Description": "Led a team to develop a mobile health application integrated with Arduino for wellness tracking."
+      },
+      {
+        "Title": "Smart Dustbin",
+        "Technologies": ["IoT"],
+        "Description": "Developed a Smart Dustbin system using Arduino microcontroller for enhanced waste management."
+      }
+    ],
+    "Achievements": [
+      "Solved 10+ problems of Machine Learning on Kaggle",
+      "4 star rating in Python on Hacker Rank"
+    ]
+  }
+}
